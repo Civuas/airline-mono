6f805469ccdbd6b2ec6b0bf647be6b7839c08373
Sauvic <sauvicpaulchy@gmail.com>
2024-05-14 18:31:13 +0530

    migrate db to sqlite

--- src/SearchService/src/config/config.js
+++ src/SearchService/src/config/config.js
@@ -1,28 +1,37 @@
 require("dotenv").config();
 
+const path = require("path");
+const storage = path.join(__dirname, "../../Search_sqlite3.db");
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_FLIGHT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_FLIGHT,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // dialect: "mysql",
+    dialect: "sqlite",
+    storage,
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_FLIGHT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_FLIGHT,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // dialect: "mysql",
+    dialect: "sqlite",
+    storage: ":memory",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_FLIGHT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_FLIGHT,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // dialect: "mysql",
+    dialect: "sqlite",
+    storage,
   },
 };
